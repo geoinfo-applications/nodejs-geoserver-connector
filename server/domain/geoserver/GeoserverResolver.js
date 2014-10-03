4079cf1cf1d362676a802345126c10484ba003ab
Ivan Grcic <ivan.grcic@geoinfo.ch>
2014-10-03 12:49:05 +0200

    refactor resolver a bit

--- server/domain/geoserver/GeoserverResolver.js
+++ server/domain/geoserver/GeoserverResolver.js
@@ -34,157 +34,172 @@ function GeoserverResolver(geoserverRepositoryConfig) {
         getStyle: "/styles/%s",
 
         getWorkspaceStyles: "/workspaces/%s/styles",
-        getWorkspaceStyle: "/workspaces/%s/styles/%s",
+        getWorkspaceStyle: "/workspaces/%s/styles/%s"
     };
+
+    this.getResolvers = {
+        resolveLayer: function (config, method) {
+
+            var restUrl = this.restAPI.getLayer;
+            var parameters = this.getParameters.getLayerParameters(config);
+
+            if (method === "create") {
+                restUrl = this.restAPI.getLayers;
+                parameters = [];
+            }
+
+            return this.formatReturnUrl(restUrl, parameters);
+        },
+
+        resolveFeatureType: function (config, method) {
+
+            var restUrl = this.restAPI.getFeatureType;
+            var parameters = this.getParameters.getFeatureTypeParameters(config);
+
+            if (this.methodIsCreateOrConfigNotExist(method, config)) {
+                restUrl = this.restAPI.getFeatureTypes;
+                parameters.pop();
+            }
+
+            return this.formatReturnUrl(restUrl, parameters);
+        },
+
+        resolveDatastore: function (config, method) {
+
+            var restUrl = this.restAPI.getDatastore;
+            var parameters = this.getParameters.getDatastoreParameters(config);
+
+            if (method === "create") {
+                restUrl = this.restAPI.getDatastores;
+                parameters.pop();
+            }
+
+            return this.formatReturnUrl(restUrl, parameters);
+        },
+
+        resolveWorkspace: function (config, method) {
+
+            var restUrl = this.restAPI.getWorkspace;
+            var parameters = this.getParameters.getWorkspaceParameters(config);
+
+            if (method === "create") {
+                restUrl = this.restAPI.getWorkspaces;
+                parameters.pop();
+            }
+
+            var formattedUrl = this.formatReturnUrl(restUrl, parameters);
+
+            return formattedUrl;
+        },
+
+        resolveWorkspaceStyle: function (config, method) {
+            var restUrl = this.restAPI.getWorkspaceStyle;
+            var parameters = this.getParameters.getWorkspaceStyleParameters(config);
+
+            if (this.methodIsCreateOrConfigNotExist(method, config)) {
+                restUrl = this.restAPI.getWorkspaceStyles;
+                parameters.pop();
+            }
+
+            return this.formatReturnUrl(restUrl, parameters);
+        },
+
+        resolveStyle: function (config, method) {
+
+            var restUrl = this.restAPI.getStyle;
+            var parameters = [];
+
+            if (config) {
+                parameters = this.getParameters.getStyleParameters(config);
+            }
+
+            if (this.methodIsCreateOrConfigNotExist(method, config)) {
+                restUrl = this.restAPI.getStyles;
+                parameters.pop();
+            }
+
+            return this.formatReturnUrl(restUrl, parameters);
+        }
+    };
+
+    this.getParameters = {
+        getLayerParameters: function (config) {
+            var workspaceName = this.resolveWorkspaceName(config);
+            return [ workspaceName, config.name ];
+        },
+
+        getFeatureTypeParameters: function (config) {
+            var datastoreName = config && config.datastore || this.datastore;
+            var workspaceName = this.resolveWorkspaceName(config);
+            return [ workspaceName, datastoreName, config.name ];
+        },
+
+        getDatastoreParameters: function (config) {
+            var datastoreName = config && config.name || this.datastore;
+            var workspaceName = this.resolveWorkspaceName(config);
+            return [ workspaceName, datastoreName ];
+        },
+
+        getWorkspaceParameters: function (config) {
+            var workspaceName = config && config.name || this.workspace;
+            return [ workspaceName ];
+        },
+
+        getWorkspaceStyleParameters: function (config) {
+            var workspaceStyleName = config && config.name || "";
+            var workspaceName = this.resolveWorkspaceName(config);
+            return [ workspaceName, workspaceStyleName ];
+        },
+
+        getStyleParameters: function (config) {
+            var styleName = config.name;
+            return [ styleName ];
+        }
+    };
+
+    _.functions(this.getParameters).forEach(function (key) {
+        this.getParameters[key] = this.getParameters[key].bind(this);
+    }, this);
+
+    _.functions(this.getResolvers).forEach(function (key) {
+        this.getResolvers[key] = this.getResolvers[key].bind(this);
+    }, this);
 
 }
 
 GeoserverResolver.prototype = {
 
+    resolveWorkspaceName: function (config) {
+        return config && config.workspace || this.workspace;
+    },
+
     methodIsCreateOrConfigNotExist: function (method, config) {
         return method === "create" || !config;
     },
 
-    formatReturnUrl: function (restApiCall, _parameters) {
+    formatReturnUrl: function (restApiCall, restParameters) {
 
-        var parameters = _parameters.slice(0);
+        var parameters = restParameters.slice(0);
         var fullRestUrl = this.baseURL + restApiCall;
 
         parameters.unshift(fullRestUrl);
         return util.format.apply(null, parameters);
     },
 
-    getLayerParameters: function (config) {
-        var workspaceName = config && config.workspace || this.workspace;
-        return [ workspaceName, config.name ];
-    },
-
-    getFeatureTypeParameters: function (config) {
-        var datastoreName = config && config.datastore || this.datastore;
-        var workspaceName = config && config.workspace || this.workspace;
-        return [ workspaceName, datastoreName, config.name ];
-    },
-
-    getDatastoreParameters: function (config) {
-        var datastoreName = config && config.name || this.datastore;
-        var workspaceName = config && config.workspace || this.workspace;
-        return [ workspaceName, datastoreName ];
-    },
-
-    getWorkspaceParameters: function (config) {
-        var workspaceName = config && config.name || this.workspace;
-        return [ workspaceName ];
-    },
-
-    getWorkspaceStyleParameters: function (config) {
-        var workspaceStyleName = config && config.name || "";
-        var workspaceName = config && config.workspace || this.workspace;
-        return [ workspaceName, workspaceStyleName ];
-    },
-
-    getStyleParameters: function (config) {
-        var styleName = config.name;
-        return [ styleName ];
-    },
-
-    resolveLayer: function (config, method) {
-
-        var restUrl = this.restAPI.getLayer;
-        var parameters = this.getLayerParameters(config);
-
-        if (method === "create") {
-            restUrl = this.restAPI.getLayers;
-            parameters = [];
-        }
-
-        return this.formatReturnUrl(restUrl, parameters);
-    },
-
-    resolveFeatureType: function (config, method) {
-
-        var restUrl = this.restAPI.getFeatureType;
-        var parameters = this.getFeatureTypeParameters(config);
-
-        if (this.methodIsCreateOrConfigNotExist(method, config)) {
-            restUrl = this.restAPI.getFeatureTypes;
-            parameters.pop();
-        }
-
-        return this.formatReturnUrl(restUrl, parameters);
-    },
-
-    resolveDatastore: function (config, method) {
-
-        var restUrl = this.restAPI.getDatastore;
-        var parameters = this.getDatastoreParameters(config);
-
-        if (method === "create") {
-            restUrl = this.restAPI.getDatastores;
-            parameters.pop();
-        }
-
-        return this.formatReturnUrl(restUrl, parameters);
-    },
-
-    resolveWorkspace: function (config, method) {
-
-        var restUrl = this.restAPI.getWorkspace;
-        var parameters = this.getWorkspaceParameters(config);
-
-        if (method === "create") {
-            restUrl = this.restAPI.getWorkspaces;
-            parameters.pop();
-        }
-
-        var formattedUrl = this.formatReturnUrl(restUrl, parameters);
-
-        return formattedUrl;
-    },
-
-    resolveWorkspaceStyle: function (config, method) {
-        var restUrl = this.restAPI.getWorkspaceStyle;
-        var parameters = this.getWorkspaceStyleParameters(config);
-
-        if (this.methodIsCreateOrConfigNotExist(method, config)) {
-            restUrl = this.restAPI.getWorkspaceStyles;
-            parameters.pop();
-        }
-
-        return this.formatReturnUrl(restUrl, parameters);
-    },
-
-    resolveStyle: function (config, method) {
-
-        var restUrl = this.restAPI.getStyle;
-        var parameters = [];
-
-        if (config) {
-            parameters = this.getStyleParameters(config);
-        }
-
-        if (this.methodIsCreateOrConfigNotExist(method, config)) {
-            restUrl = this.restAPI.getStyles;
-            parameters.pop();
-        }
-
-        return this.formatReturnUrl(restUrl, parameters);
-    },
-
-    "delete": function (type, config) {
+    delete: function (type, config) {
         return this.get(type, config, "delete");
     },
 
-    "create": function (type, config) {
+    create: function (type, config) {
         return this.get(type, config, "create");
     },
 
-    "get": function (type, config, method) {
+    get: function (type, config, method) {
 
         if (!method) {
             method = "get";
         }
-
-        return this["resolve" + type](config, method);
+        return this.getResolvers["resolve" + type](config, method);
     }
 };
 
