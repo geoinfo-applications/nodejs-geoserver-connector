52c03e85deaaa8af9a095236fc457839dd218247
Ivan Grcic <ivan.grcic@geoinfo.ch>
2014-10-15 09:30:36 +0200

    add clover coverage format for bamboo

--- gruntfile.js
+++ gruntfile.js
@@ -93,6 +93,7 @@ module.exports = function (grunt) {
                 src: [ "./server/test/**" ],
                 options: {
                     reporter: "mocha-multi",
+                    reportFormats: [ "lcov", "clover" ],
                     //  excludes: ["**/integration/"],
                     recursive: true,
                     print: "summary",
